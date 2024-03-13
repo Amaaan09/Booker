d892789712ef4ee24464f8f54a596dbedfa1534c
Amaan Ansari <m.amaan02@gmail.com>
2024-03-13 10:38:02 +0530

    All dependencies done, Trial for runner, Basic App Streamlit

--- runner.py
+++ runner.py
@@ -17,15 +17,15 @@ documents = loader.load()
 text_splitter = CharacterTextSplitter(
     chunk_size=1000, chunk_overlap=30, separator="\n"
 )
+
 docs = text_splitter.split_documents(documents=documents)
 
 embeddings = HuggingFaceEmbeddings()
 vectorstore = FAISS.from_documents(docs, embeddings)
-vectorstore.save_local("faiss_index_react")
 
-new_vectorstore = FAISS.load_local("faiss_index_react", embeddings)
+
 qa = RetrievalQA.from_chain_type(
-    llm=llm, chain_type="stuff", retriever=new_vectorstore.as_retriever()
+    llm=llm, chain_type="stuff", retriever=vectorstore.as_retriever()
 )
 res = qa.run("Give me the gist of ReAct in 3 sentences")
 print(res)
